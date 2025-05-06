ebe44922d479148222e6626b21cfaeaab5cfafbb
Abdullah <abdullahawan7701@gmail.com>
2025-05-07 01:09:27 +0500

    Updated

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
     ],
     esmExternals: "loose",
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) return config;
+
+    config.module.rules.push({
+      test: /node_modules[\/\\](@puppeteer\/browsers|extract-zip)/,
+      use: "ignore-loader",
+    });
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      path: false, // Disable path resolution at build time
+      fs: false,   // Disable fs resolution at build time
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
